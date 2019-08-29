2e25113f8e8393119227ff2af42dc8d5489de59a
Kathleen Mullins <mullinsk@ccs.neu.edu>
2019-08-29 19:50:18 -0400

    adding comments + style

--- template-parts/content-post.php
+++ template-parts/content-post.php
@@ -79,6 +79,12 @@
 			'after'  => '</div>',
 		) );
 		?>
+		<?php 
+		// If comments are open or we have at least one comment, load up the comment template.
+			if ( comments_open() && get_comments_number() ) {
+				comments_template();
+			}
+		?>
 	</div><!-- .entry-content -->
 
 	<footer class="entry-footer cfr-wrapper">
